e7d24800244b5b8dad873703a3d9865a5b690342
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-09 00:25:43 +0530

    ADDED : Code to store daily wage along total wage

--- EmployeeWage/EmployeeWageCalculator.js
+++ EmployeeWage/EmployeeWageCalculator.js
@@ -1,5 +1,3 @@
-
-
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -18,12 +16,22 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+
+function calculateWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let dailyWageArray = new Array();
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    dailyWageArray.push(calculateWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+
+let empWage = calculateWage(totalEmpHrs);
+console.log(dailyWageArray.toString());
 console.log("Total Days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs + " Emp Wage : " + empWage);
