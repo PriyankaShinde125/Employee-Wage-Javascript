51b0d926193ff372df804b87435811bb9dd53249
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-08 23:59:32 +0530

    ADDED : Code to check employee attendance

--- EmployeeWage/EmployeeWageCalculator.js
+++ EmployeeWage/EmployeeWageCalculator.js
@@ -1,1 +1,9 @@
-console.log("Welcome to employee wage calculator")
+//UC1_Check employee attendance
+const IS_ABSENT = 0;
+let attendance = Math.floor(Math.random() * 10) % 2;
+if (attendance == IS_ABSENT) {
+    console.log("Employee is ABSENT");
+}
+else {
+    console.log("Employee is PRESENT");
+}
